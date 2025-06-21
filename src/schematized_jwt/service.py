8bad7e63afa6fe54770e7b69434a55a0c6c36e72
Vladimir Boltushev <vladimirboltushev@yandex.ru>
2025-06-21 22:29:54 +0300

    Add encode methods

--- src/schematized_jwt/service.py
+++ src/schematized_jwt/service.py
@@ -1,5 +1,9 @@
 import datetime
+import time
+from enum import StrEnum
 from typing import Annotated, Generic, Literal, TypeVar
+
+import jwt
 
 from .schemas import JWTSchema
 
@@ -11,6 +15,80 @@ type TokenTTL = Annotated[
     The validity period of the token, if None is specified, the token has no expiration date.
     """,
 ]
+type JWTToken = Annotated[str, "Encoded JWT token."]
+
+
+class TokenType(StrEnum):
+    ACCESS = "access"
+    REFRESH = "refresh"
+
+
+class PayloadBuilder(Generic[TS]):
+    def __init__(
+        self,
+        access_ttl: TokenTTL,
+        refresh_ttl: TokenTTL,
+    ) -> None:
+        self.access_ttl = access_ttl
+        self.refresh_ttl = refresh_ttl
+
+    def _add_exp(self, payload: dict, token_type: TokenType):
+        token_ttl = {
+            TokenType.ACCESS: self.access_ttl,
+            TokenType.REFRESH: self.refresh_ttl,
+        }[token_type]
+        if token_ttl:
+            now = int(time.time())
+            token_exp = now + token_ttl.seconds
+            payload.update({"exp": token_exp})
+
+    def _add_token_type(self, payload: dict, token_type: TokenType):
+        payload.update({"type": token_type})
+
+    def payload_build(
+        self,
+        payload: TS,
+        token_type: TokenType,
+    ) -> dict:
+        payload_with_tech_data = payload.model_dump()
+        self._add_exp(
+            payload=payload_with_tech_data,
+            token_type=token_type,
+        )
+        self._add_token_type(
+            payload=payload_with_tech_data,
+            token_type=token_type,
+        )
+        return payload_with_tech_data
+
+
+class JWTEncoder(Generic[TS]):
+    def __init__(
+        self,
+        payload_builder: PayloadBuilder,
+        secret_key: str,
+        algorithm: JWTEncodeAlgorithm,
+    ) -> None:
+        self.payload_builder = payload_builder
+        self.secret_key = secret_key
+        self.algorithm = algorithm
+
+    def _base_encode(self, payload, token_type: TokenType) -> JWTToken:
+        payload_with_tech_data = self.payload_builder.payload_build(
+            payload=payload,
+            token_type=token_type,
+        )
+        return jwt.encode(
+            payload=payload_with_tech_data,
+            key=self.secret_key,
+            algorithm=self.algorithm,
+        )
+
+    def encode_access(self, payload: TS) -> JWTToken:
+        return self._base_encode(payload=payload, token_type=TokenType.ACCESS)
+
+    def encode_refresh(self, payload: TS) -> JWTToken:
+        return self._base_encode(payload=payload, token_type=TokenType.REFRESH)
 
 
 class BaseJWTService(Generic[TS]):
@@ -32,9 +110,32 @@ class BaseJWTService(Generic[TS]):
     secret_key: Annotated[
         str,
         "The secret key used for signing and verifying JWT tokens.",
-    ]
+    ] = NotImplemented
     algorithm: JWTEncodeAlgorithm = "HS256"
 
     # Tokens TTL
     access_ttl: TokenTTL = datetime.timedelta(minutes=30)
     refresh_ttl: TokenTTL = datetime.timedelta(days=7)
+
+    @classmethod
+    def get_payload_builder(cls) -> PayloadBuilder:
+        return PayloadBuilder(
+            access_ttl=cls.access_ttl,
+            refresh_ttl=cls.refresh_ttl,
+        )
+
+    @classmethod
+    def get_jwt_encoder(cls) -> JWTEncoder:
+        return JWTEncoder(
+            payload_builder=cls.get_payload_builder(),
+            secret_key=cls.secret_key,
+            algorithm=cls.algorithm,
+        )
+
+    @classmethod
+    def encode_access(cls, payload: TS) -> JWTToken:
+        return cls.get_jwt_encoder().encode_access(payload)
+
+    @classmethod
+    def encode_refresh(cls, payload: TS) -> JWTToken:
+        return cls.get_jwt_encoder().encode_access(payload)
